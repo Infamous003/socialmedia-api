b05fddaa7e1b6d1c54e1bc8f9254c12b91f953c4
Infamous003 <arfathsyed0312@gmail.com>
2025-03-06 18:55:40 +0530

    Add endpoints for retreiving posts

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
 @app.get("/")
 def welcome():
     return {"message": "Welcome to social media API!"}
+
+POSTS = [
+    {"id": 1, "title": "Shutter Island | Movie breakdown", "description": "Shutter Island is a psychological thriller by none other than the great producer Christopher Nolan..."},
+    {"id": 2, "title": "Batman 2022 Review", "description": "After over a decade of waiting, we have finally come back to Gotham..."},
+    {"id": 3, "title": "A Separation", "description": "The Iranian Oscar winning movie is about a family which is going through divorce but something unexpected happens..."},
+]
+
+@app.get("/posts")
+def get_posts():
+    return POSTS
+
+@app.get("/posts/{id}")
+def get_posts(id: int):
+    post_exists = [post for post in POSTS if post["id"] == id]
+    if not post_exists:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return post_exists
