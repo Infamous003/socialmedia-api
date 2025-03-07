d0ae7f4db4d12f0d34e5bf921f848a66bddda676
Infamous003 <arfathsyed0312@gmail.com>
2025-03-07 18:50:49 +0530

    Add different pydantic models for each request

--- models.py
+++ models.py
@@ -4,3 +4,14 @@ class Post(BaseModel):
     id: int
     title: str
     description: str
+
+class PostCreate(BaseModel):
+    title: str
+    description: str
+
+class PostPublic(BaseModel):
+    title: str
+    description: str
+
+class PostUpdate(PostCreate):
+    pass
