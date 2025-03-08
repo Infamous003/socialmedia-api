b6558132343ed3d2c3a603c0d7498c834edbc4b2
Infamous003 <arfathsyed0312@gmail.com>
2025-03-08 13:28:58 +0530

    Add pydantic models for Users

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from sqlmodel import SQLModel, Field, String
 
 class Post(SQLModel, table=True):
@@ -17,4 +17,25 @@ class PostPublic(BaseModel):
 class PostUpdate(PostCreate):
     title: str | None = None
     description: str | None = None
+
+# USER Models
+
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(String(32), nullable=False, unique=True)
+    email: EmailStr = Field(String(128), nullable=False, unique=True)
+    hashed_password: str = Field(nullable=False)
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+
+class UserUpdate(BaseModel):
+    username: str | None = None
+    password: str | None = None
 
